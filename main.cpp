e6526aa1da5f2b2869f547dc7b679f5be7b52e90
Roy QIU <karoyqiu@gmail.com>
2022-12-28 10:37:29 +0800

    添加图标。

--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
 #include "stable.h"
-#include "mainform.h"
+#include "mainwindow.h"
+
+
+static void checkDarkMode(QApplication &app)
+{
+#ifdef Q_OS_WIN
+    QSettings settings(QS(R"(HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize)"), QSettings::NativeFormat);
+
+    if (settings.value("AppsUseLightTheme") == 0)
+    {
+        app.setStyle(QStyleFactory::create(QS("Fusion")));
+
+        QPalette darkPalette;
+        QColor darkColor(45, 45, 45);
+        QColor disabledColor(127, 127, 127);
+        QColor textColor(204, 204, 204);
+        darkPalette.setColor(QPalette::Window, darkColor);
+        darkPalette.setColor(QPalette::WindowText, textColor);
+        darkPalette.setColor(QPalette::Base, QColor(18, 18, 18));
+        darkPalette.setColor(QPalette::AlternateBase, darkColor);
+        darkPalette.setColor(QPalette::ToolTipBase, textColor);
+        darkPalette.setColor(QPalette::ToolTipText, textColor);
+        darkPalette.setColor(QPalette::Text, textColor);
+        darkPalette.setColor(QPalette::Disabled, QPalette::Text, disabledColor);
+        darkPalette.setColor(QPalette::Button, darkColor);
+        darkPalette.setColor(QPalette::ButtonText, textColor);
+        darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, disabledColor);
+        darkPalette.setColor(QPalette::BrightText, Qt::red);
+        darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+        darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+        darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+        darkPalette.setColor(QPalette::Disabled, QPalette::HighlightedText, disabledColor);
+
+        app.setPalette(darkPalette);
+
+        app.setStyleSheet(QS("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }"));
+    }
+#else
+    Q_UNUSED(app);
+#endif
+}
 
 
 int main(int argc, char *argv[])
@@ -14,8 +54,11 @@ int main(int argc, char *argv[])
     QApplication::setOrganizationDomain(QS("karoyqiu.gmail.com"));
     QApplication::setApplicationDisplayName(QApplication::translate("main", "qtail"));
 
-    MainForm w;
+    checkDarkMode(a);
+    QIcon::setThemeName(QS("elementary"));
+
+    MainWindow w;
     w.show();
 
-    return a.exec();
+    return QApplication::exec();
 }
--- qtail_zh_CN.ts
+++ qtail_zh_CN.ts
@@ -2,9 +2,36 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="21"/>
+        <location filename="out/build/x64-Debug/qtail_autogen/include/ui_mainwindow.h" line="61"/>
+        <source>Main</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="37"/>
+        <location filename="out/build/x64-Debug/qtail_autogen/include/ui_mainwindow.h" line="56"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <location filename="out/build/x64-Debug/qtail_autogen/include/ui_mainwindow.h" line="58"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="48"/>
+        <location filename="out/build/x64-Debug/qtail_autogen/include/ui_mainwindow.h" line="60"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>main</name>
     <message>
-        <location filename="main.cpp" line="15"/>
+        <location filename="main.cpp" line="55"/>
         <source>qtail</source>
         <translation type="unfinished"></translation>
     </message>
